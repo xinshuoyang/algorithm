8debaaebcd6f18b4ae1a1fb3db30f547116c3b26
Xinshuo Yang <xinshuoyang@gmail.com>
2017-12-05 15:30:58 -0700

    573

--- src/573_BuildPostOfficeII/solution.py
+++ src/573_BuildPostOfficeII/solution.py
@@ -10,6 +10,7 @@
 # DATE : 20171201
 #
 ################################################################################
+import sys
 
 class Solution:
     """
@@ -18,6 +19,7 @@ class Solution:
     """
     def shortestDistance(self, grid):
         # write your code here
+        
         m = len(grid)
         if m == 0:
             return 0
@@ -25,75 +27,64 @@ class Solution:
         n = len(grid[0])
         if n == 0:
             return 0
-
-        postoffice = {}
+        
+        dist = {}
+        po = []
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    postoffice[i, j] = False
+                    dist[i, j] = [[sys.maxint for k in range(n)] for l in range(m)]
+                    self.dist(i, j, grid, dist[i, j])
 
-        dist = []
+                    po.append([i, j])
+        
+        sdist = []
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    dist.append(self.distsum(i, j, grid, postoffice))
-                    self.postofficeini(postoffice)
-        
-        if max(dist) >= 0:
-            sdist = max(dist)
-            for dd in dist:
-                if dd >= 0 and dd < sdist:
-                    sdist = dd
-            return sdist
-        else:
+                    dd = 0
+                    for k, l in po:
+                        if dist[k, l][i][j] == sys.maxint:
+                            dd = sys.maxint
+                            break
+                        else:
+                            dd += dist[k, l][i][j]
+                    sdist.append(dd)
+        if min(sdist) == sys.maxint:
             return -1
-
-    def distsum(self, x, y, grid, postoffice):
+        else:
+            return min(sdist) 
+       
+    def dist(self, x, y, grid, dist):
         """
-        find the sum of distances from (x, y) to all post offices
+        find distance from (x, y) to all empty(=0) point
         """
-        dist = 0
-
         m = len(grid)
         n = len(grid[0])
-        
-        visit = [[False for i in range(n)] for j in range(m)]
 
+        visit = [[False for i in range(n)] for j in range(m)]
+        
         q1 = [[x, y]]
-        visit[x][y] = True
 
         step = 1
-        while len(q1) != 0:
+        while q1:
             q2 = []
             for i, j in q1:
-                for k, l in ((i - 1, j), (i + 1, j), (i , j - 1), (i, j + 1)):
-                    if k > -1 and k < m and l > -1 and l < n:
-                        if grid[k][l] == 0:
-                            if not visit[k][l]:
-                                visit[k][l] = True
-                                q2.append([k, l])
-                        elif grid[k][l] == 1:
-                            if not postoffice[k, l]:
-                                dist += step
-                                postoffice[k, l] = True
+                for k, l in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                    if k >= 0 and k < m and l >= 0 and l < n\
+                            and not visit[k][l] and grid[k][l] == 0:
+                        dist[k][l] = step
+                        visit[k][l] = True
+                        q2.append((k, l))
+
             q1 = q2
             step += 1
-        
-        for pp in postoffice:
-            if not postoffice[pp]:
-                return -1
+            
         return dist
-
-    def postofficeini(self, postoffice):
-        """
-        initialize postoffice to be False
-        """
-        for pp in postoffice:
-            postoffice[pp] = False
-        return
-
+    
 if __name__ == '__main__':
     s = Solution()
     grid = [[0,1,0,0,0],[1,0,0,2,1],[0,1,0,0,0]]
-
+    #dist = [[sys.maxint for i in range(5)] for i in range(3)]
+    #print s.dist(0, 1, grid, dist)
     print s.shortestDistance(grid)
