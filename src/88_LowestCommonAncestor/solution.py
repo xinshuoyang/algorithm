7b90b9eb80daae6140cb0e1a9a69971a2473e136
xinshuoyang <xinshuoyang@gmail.com>
2017-12-07 21:57:54 -0700

    88

--- src/88_LowestCommonAncestor/solution.py
+++ src/88_LowestCommonAncestor/solution.py
@@ -29,29 +29,63 @@ class Solution:
     def lowestCommonAncestor(self, root, A, B):
         # write your code here
 
+        # find paths to A and B
+        pathA = []
+        pathB = []
+
+        self.findpath(root, A, pathA)
+        self.findpath(root, B, pathB)
+
+        res = root
+
+        for i in range(min(len(pathA), len(pathB))):
+            if pathA[i] is pathB[i]:
+                res = pathA[i]
+            else:
+                break
+
+        return res 
+
+    def findpath(self, root, node, path):
+        """
+        find path from root to node
+        """
+
         if root is None:
-            return None
-            
-        if root is A or root is B:
-            return root
-            
-        left = self.lowestCommonAncestor(root.left, A, B)
-        right = self.lowestCommonAncestor(root.right, A, B)
-        
-        if left is not None and right is not None:
-            return root
-        if left is not None:
-            return left
-        if right is not None:
-            return right
-        return Non                
+            return False
+        else:
+            if root is node:
+                path.append(root)
+                return True
+            else:
+                found = False
+
+                path.append(root)
+
+                if root.left is not None:
+                    found = self.findpath(root.left, node, path)
+
+                if found:
+                    return True
+
+                if root.right is not None:
+                    found = self.findpath(root.right, node, path)
+
+                if found:
+                    return True
+                else:
+                    path.pop()
+
+                return False
+
+
 
 if __name__ == '__main__':
-    root = TreeNode(1)
-    root.left = TreeNode(2)
-    root.right = TreeNode(3)
+    root = TreeNode(4)
+    root.left = TreeNode(3)
+    root.right = TreeNode(7)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(6)
 
     s = Solution()
-    
-    print s.lowestCommonAncestor(root, root.left, root.right).val
-
+    print s.lowestCommonAncestor(root, root.left, root.right.left).val
